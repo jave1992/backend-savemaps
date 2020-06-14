cc7879458f5b347300819fccfc89e9f030e718f1
Jair Vasquez <jair1624@gmail.com>
2020-06-14 18:13:04 -0500

    Conexion a MongoDB

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var uploadRoutes = require('./routes/upload');
 var imagenesRoutes = require('./routes/imagenes');
 
 // Conexión a la base de datos
-mongoose.connection.openUri('mongodb://localhost:27017/mongodb+srv://mymapsadmin:mymapspassword@cluster0-zsv0g.mongodb.net/mymapsDB?retryWrites=true&w=majority', (err, res) => {
+mongoose.connection.openUri('mongodb+srv://mymapsadmin:mymapspassword@cluster0-zsv0g.mongodb.net/mymapsDB?retryWrites=true&w=majority', (err, res) => {
     if (err) throw err;
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 });
